b77d6beb055658d2d8176b6b642c0a07ee6f8c95
Alexey Poliachinscky <polya4inscky.alex@yandex.ru>
2020-01-05 22:29:14 +0300

    strings task resolved

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -242,8 +242,12 @@ function getRectangleString(width, height) {
  *
  */
 function encodeToRot13(str) {
-    throw new Error('Not implemented');
+    //throw new Error('Not implemented');
+    let string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    let rot13 = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+return str.split('').map(i => string.indexOf(i) == -1 ? i :  rot13[string.indexOf(i)]).join('')
 }
+
 
 /**
  * Returns true if the value is string; otherwise false.
@@ -259,7 +263,8 @@ function encodeToRot13(str) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-    throw new Error('Not implemented');
+    return typeof value == 'string' || value instanceof String ? true : false
+    //throw new Error('Not implemented');
 }
 
 
@@ -288,7 +293,12 @@ function isString(value) {
  *   'K♠' => 51
  */
 function getCardId(value) {
-    throw new Error('Not implemented');
+    //throw new Error('Not implemented');
+    let card = [ 'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
+ 'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
+ 'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
+ 'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠']
+ return card.indexOf(value)
 }
 
 
